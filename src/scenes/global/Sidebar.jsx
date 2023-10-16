a75b1ab7f23723b0bf2bdd0d0b100cbe39ff4316
Thuma-tec <thumablood@gmail.com>
2023-10-17 01:17:06 +0530

    Importing on sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,3 +1,25 @@
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useState } from "react";
+import {ProSidebar, Menu, MenuItem} from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Link } from "react-router-dom";
+import{ tokens } from "../../theme";
+import HomeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+
 
 const Sidebar = () => {
     return (
