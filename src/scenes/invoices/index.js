a780361d191be80a3d007fcf4ad3c9c89b10215e
Thuma-tec <thumablood@gmail.com>
2024-02-11 22:36:26 +0530

    Contacts component - Mui data grid, inbuild data grid classes

--- src/scenes/invoices/index.js
+++ src/scenes/invoices/index.js
@@ -1,19 +1,17 @@
-import { Box, IconButton, Typography, colors, useTheme } from "@mui/material";
-import { mockDataTeam } from "../../data/mockData";
+import { Box, colors, useTheme, Typography } from "@mui/material";
+import { mockDataInvoices } from "../../data/mockData";
 import { tokens } from "../../theme";
-import { DataGrid } from "@mui/x-data-grid";
-import { AdminPanelSettingsOutlined } from "@mui/icons-material";
-import { LockOpenOutlined } from "@mui/icons-material";
-import { SecurityOutlined } from "@mui/icons-material";
+import { DataGrid} from "@mui/x-data-grid";
 import Header from "../../components/Header";
 
-const Team = () => {
+const Invoices = () => {
 
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
     const columns = [
-        { field: "id", headerName: "ID" },
+        { field: "id", headerName: "ID", flex:0.5 },
+        { field: "registrarId", headerName: "Register ID"},
         {
             field: "name",
             headerName: "Name",
@@ -38,44 +36,29 @@ const Team = () => {
             flex: 1
         },
         {
-            field: "access",
-            headerName: "Access Level",
-            flex: 1,
-            renderCell: ({ row: { access } }) => { // row: { access} means grab the row first then grab access
-
-                return (
-                    <Box
-                        width="60%"
-                        m="0 auto"
-                        p="5px"
-                        display="flex"
-                        justifyContent="center"
-                        backgroundColor={
-                            access === "admin"
-                                ? colors.greenAccent[600]
-                                : colors.greenAccent[700]
-                        }
-                        borderRadius="4px"
-                    >
-                        {access === "admin" && <AdminPanelSettingsOutlined />}
-                        {access === "manager" && <SecurityOutlined />}
-                        {access === "user" && <LockOpenOutlined />}
-                        <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
-                            {access}
-                        </Typography>
-
-                    </Box>
-                )
-            }
-        }
-    ]
+            field: "address",
+            headerName: "Address",
+            flex: 1
+        },
+        {
+            field: "city",
+            headerName: "City",
+            flex: 1
+        },
+        {
+            field: "zipCode",
+            headerName: "ZipCode",
+            flex: 1
+        },
+       
+    ];
 
     return (
 
         <Box m="20px">
             <Header
-                title="Team"
-                subtitle="Managing Team Members"
+                title="CONTACTS"
+                subtitle="List of contacts for future preferences"
             />
 
             <Box
@@ -101,12 +84,16 @@ const Team = () => {
                     "& MuiDataGrid-footerContainer": {
                         borderTop: "none",
                         backgroundColor: colors.blueAccent[700]
-                    }
+                    },
+                    "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                        color: `${colors.grey[100]} !important`
+                }
                 }}
             >
                 <DataGrid
-                    rows={mockDataTeam}
+                    rows={mockDataContacts}
                     columns={columns}
+                    components={{Toolbar: GridToolbar}}  // filter option
                 />
             </Box>
 
@@ -114,4 +101,4 @@ const Team = () => {
     );
 };
 
-export default Team;
+export default Invoices;
